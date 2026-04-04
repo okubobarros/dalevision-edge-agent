fc594546bd0f152bc1baf4ca5cd5aeb83e47ca1a
Okubo Barros <okubobarros@gmail.com>
2026-04-04 11:59:29 -0300

    bin del

--- bin/render_build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-# Stub build script for Render
-# If a real build is needed, replace with actual steps.
-if [ -f ./scripts/render_build.sh ]; then
-  ./scripts/render_build.sh "$@"
-  exit $?
-fi
-echo 'render_build.sh: no-op (edge agent repo)'
